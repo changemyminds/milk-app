ad2aec7e15a48a78ecc474679b954249d6a72868
changemyminds <david7717318@gmail.com>
2024-02-12 23:51:40 +0800

    refactor: refactor scheduler

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.adapter.inbound.line_controller import linebot_bp
-from app.adapter.inbound.scheduler import count_day_milk_job
+from app.adapter.inbound.audit_scheduler import count_day_milk_job
 import logging
 
 log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -16,5 +16,5 @@ app.register_blueprint(linebot_bp)
 # scheduler run
 logging.info("scheduler run")
 scheduler = BackgroundScheduler()
-scheduler.add_job(count_day_milk_job, 'cron', day='*', hour=1, minute=0, timezone='Asia/Taipei')
+scheduler.add_job(count_day_milk_job, 'cron', day='*', hour=0, minute=1, timezone='Asia/Taipei')
 scheduler.start()
