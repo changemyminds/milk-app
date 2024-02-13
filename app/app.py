1a7f18ffc659dd9833d329215cf3e424c563f173
changemyminds <david7717318@gmail.com>
2024-02-13 16:59:30 +0800

    feat(log): updated the log format

--- app/app.py
+++ app/app.py
@@ -4,9 +4,10 @@ from app.adapter.inbound.line_controller import linebot_bp
 from app.adapter.inbound.audit_scheduler import count_day_milk_job
 import logging
 
-log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-
-logging.basicConfig(level=logging.INFO, format=log_format)
+log_format = '%(asctime)s.%(msecs)03dZ %(levelname)s [%(name)s] <%(thread)d> - %(filename)s:%(funcName)s():(%(lineno)d) - %(message)s'
+log_time_format = '%Y-%m-%d %H:%M:%S'
+logging.basicConfig(level=logging.INFO, format=log_format,
+                    datefmt=log_time_format)
 
 # flask run
 logging.info("flask run")
@@ -16,5 +17,6 @@ app.register_blueprint(linebot_bp)
 # scheduler run
 logging.info("scheduler run")
 scheduler = BackgroundScheduler()
-scheduler.add_job(count_day_milk_job, 'cron', day='*', hour=0, minute=1, timezone='Asia/Taipei')
+scheduler.add_job(count_day_milk_job, 'cron', day='*',
+                  hour=0, minute=1, timezone='Asia/Taipei')
 scheduler.start()
